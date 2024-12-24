a6e494233ce2005f6fab40232d146e01d618ef6d
SWHL <liekkaskono@163.com>
2024-12-24 09:24:27 +0800

    chore: remove invalid files

--- demo.py
+++ demo.py
@@ -11,7 +11,6 @@ img_path = "tests/test_files/PMC3576793_00004.jpg"
 img = cv2.imread(img_path)
 
 boxes, scores, class_names, elapse = layout_engine(img_path)
-print(boxes.shape)
 ploted_img = VisLayout.draw_detections(img, boxes, scores, class_names)
 if ploted_img is not None:
     cv2.imwrite("layout_res.png", ploted_img)
